acbde6bd49138a456e0c35f09d1796d7740c2ff1
PilotEddie <150688914+PilotEddie@users.noreply.github.com>
2024-11-25 22:30:17 +0100

    Update Actuation_calculation_gear.py

--- Structures_scripts/Actuation_calculation_gear.py
+++ Structures_scripts/Actuation_calculation_gear.py
@@ -2,10 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Constants
+g=9.81
 m_rod = 1  # mass
 m_motor = 10 # mass at end of beam
 L = 1  # length
 I = (1/3) * m_rod * L**2+ m_motor*L**2  # Moment of inertia
+
+
+#------FIRST CASE: EXTENSION------#
 
 theta_f = (117/180) * np.pi  # Final angle to cover
 t_f = 30  # Total time  
@@ -47,3 +51,14 @@ plt.show()
 
 print("The maximum torque required is:", np.max(torque_t))
 print("The maximum angle is:", np.rad2deg(np.max(theta_t)))
+
+#------SECOND CASE: SUSTAINMENT------#
+Moment=(m_rod*1/2*L+m_motor*L)*g
+#Maximum torque required to sustain the load
+print("The maximum torque required to sustain the load with a safety factor of 1.2 is:", max(Moment, np.max(torque_t)))
+
+#REQUIRED GEARING TO ACHIEVE THIS WITH A MOTOR OF 10 Nm
+Motor_torque = 10
+# Gear ratio
+gear_ratio = Moment/Motor_torque
+print("The gear ratio required to achieve this with a motor of 10 Nm is:", gear_ratio)
